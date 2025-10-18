bab9dd11fc829fedea14f33186e219f3d86deef4
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-10-18 19:11:34 +0530

    Added backaground video

--- pka_frontend/src/App.jsx
+++ pka_frontend/src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Chatbot from './Chatbot'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chatbot from "./Chatbot";
+import BG_VID from "./assets/BG_VID.mp4";
+import "./App.css";
 function App() {
-   
-const navigate=useNavigate();
   return (
     <>
-        <BrowserRouter>
-         <Routes>
-          <Route path='/chatbot' element="Chatbot">
-
-          </Route>
-         </Routes>
-        </BrowserRouter>
+      <div className="bg-video-wrap">
+        <video autoPlay loop muted playsInline className="bg-video">
+          <source src={BG_VID} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
