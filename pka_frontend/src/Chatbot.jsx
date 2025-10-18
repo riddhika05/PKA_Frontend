ec0c41a94eca0bccf38c4bd0c81ce96c6f2b6e0d
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-10-18 22:06:58 +0530

    Added video as bg to chatbot

--- pka_frontend/src/Chatbot.jsx
+++ pka_frontend/src/Chatbot.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import "./App.css"
+import BG_VID from "./assets/BG_VID.mp4";
 const Chatbot = () => {
   return (
-    <div>
-      
-    </div>
+   <div className="bg-video-wrap">
+           <video
+             autoPlay
+             loop
+             muted
+             playsInline
+             className="bg-video"
+             onLoadedData={() => console.log("Video loaded successfully")}
+             onError={(e) => console.log("Video error:", e)}
+           >
+             <source src={BG_VID} type="video/mp4" />
+             Your browser does not support the video tag.
+           </video>
+         </div>
   )
 }
 
